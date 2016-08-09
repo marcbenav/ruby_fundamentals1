1c7946431e70760a45704d34d7099dcbecd28611
marcbenav <m.benav86@gmail.com>
2016-08-09 14:46:06 -0400

    excersize Q's 1-4

---
+++ excersize2.rb
@@ -0,0 +1,4 @@
+puts "To calculate tip #{0.18 * 55}"
+puts "This is how we use strings and intergers #{9+3}"
+puts " This is an example if string interpolation #{45628*7839}"
+puts (10 < 20 && 30 < 20) || ! (10==11)
