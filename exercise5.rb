2dabd572937c8a0f24c5484138ba6b449e488554
marcbenav <m.benav86@gmail.com>
2016-08-13 16:07:16 -0400

    added user input for F

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,1 @@
-puts "What is the current temp in F?:"
-t = gets.chomp
-def cel(t)
-  return (t.to_i - 32) * 5/9
-end
-
-puts "The current temp is Celsius is #{cel(t)}"
+puts " What is the current tempreture in Fahrenheit? :"
